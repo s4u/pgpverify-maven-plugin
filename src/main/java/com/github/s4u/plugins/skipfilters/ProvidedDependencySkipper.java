bfab2a35ddaf349e8ff11a984dac6f32932ffc31
Kortanul <kortanul@protonmail.com>
2018-05-23 23:00:46 -0400

    Address PMD Feedback about Mojo God Class
    the `shouldSkipArtifact` method was so complex that any additional changes caused it to be flagged by PMD as being too complex, and the class to be flagged as a "god class". this re-factors the logic for skipping artifacts to leverage a set of strategy classes instead.

---
+++ src/main/java/com/github/s4u/plugins/skipfilters/ProvidedDependencySkipper.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018 Wren Security.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.s4u.plugins.skipfilters;
+
+import org.apache.maven.artifact.Artifact;
+
+/**
+ * A filter that always skips verification of runtime-provided dependencies.
+ */
+public class ProvidedDependencySkipper implements SkipFilter {
+    @Override
+    public boolean shouldSkipArtifact(Artifact artifact) {
+        final String artifactScope = artifact.getScope();
+
+        return artifactScope != null && artifactScope.equals(Artifact.SCOPE_PROVIDED);
+    }
+}
---
+++ src/main/java/com/github/s4u/plugins/skipfilters/SkipFilter.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Wren Security.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.s4u.plugins.skipfilters;
+
+import org.apache.maven.artifact.Artifact;
+
+/**
+ * An interface for a filter that determines whether or not a particular artifact should be
+ * processed or skipped, based on the mojo configuration.
+ */
+public interface SkipFilter {
+    /**
+     * Indicates whether or not an artifact should be skipped, based on the configuration of this
+     * filter.
+     *
+     * @param   artifact
+     *          The artifact being considered for verification.
+     *
+     * @return  {@code true} if the artifact should be skipped; {@code false} if it should be
+     *          processed.
+     */
+    boolean shouldSkipArtifact(final Artifact artifact);
+}
