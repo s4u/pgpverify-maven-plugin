f186df10f631242341b39bfa76bd756e70376934
Kortanul <kortanul@protonmail.com>
2018-05-29 09:15:29 -0400

    Fix handling of bundle and test-jar packaging
    Changes the skip filter for reactor dependencies to only compare based on Maven coordinates rather than the full ID of each artifact. Previously, the group ID, artifact ID, version, packaging, type, and classifier had to match, but unfortunately Maven doesn't have packaging information when resolving upstream reactor dependencies. This caused problems for `bundle` and `test-jar` packaging (which both end up being "jar" packaging under the hood). The new approach just uses coordinates of group ID, artifact ID, and version; this should work fine since it's unlikely that one would want a dependency outside the reactor that exactly matches the coordinates of a project inside the reactor during the same build.

---
+++ src/it/reactorProcessPom/project-pom/src/main/java/org/simplify4u/plugins/Main.java
@@ -0,0 +1,7 @@
+package org.simplify4u.plugins;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a fake class.");
+    }
+}
---
+++ src/it/reactorProcessTestJar/project-test-jar/src/test/java/org/simplify4u/plugins/MainTest.java
@@ -0,0 +1,7 @@
+package org.simplify4u.plugins;
+
+class MainTest {
+    public static void main(String[] args) {
+        System.out.println("This is a fake test class.");
+    }
+}
