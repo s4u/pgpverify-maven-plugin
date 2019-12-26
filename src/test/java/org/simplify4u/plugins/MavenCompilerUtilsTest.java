49f9f6a37d912455862fa85145badda6733a61dc
Danny van Heumen <danny@dannyvanheumen.nl>
2019-12-26 23:39:15 +0100

    Fix copyright year for new files.

--- src/test/java/org/simplify4u/plugins/MavenCompilerUtilsTest.java
+++ src/test/java/org/simplify4u/plugins/MavenCompilerUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Danny van Heumen
+ * Copyright 2019 Danny van Heumen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
